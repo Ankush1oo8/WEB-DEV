32aaf6f282507586f0bd78c152c0f51f9f675ad2
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-04-03 02:05:43 +0530

    basics of javaScript

---
+++ DAY 14/basics.js
@@ -0,0 +1,37 @@
+a=10;
+age=35;
+Name='ankush';
+adult=true;
+console.log(typeof a,typeof age,typeof Name,typeof adult);
+
+//number in js
+//detects type automatically of variable
+
+//operations
+//addition
+sum=10+15;
+console.log(sum);
+
+//subtration
+sub=sum-16;
+console.log(sub);
+
+//multiplication
+mul=sub*sum;
+console.log(mul);
+
+//division
+div=15/5;
+console.log(div);
+
+//modulo
+rem=44%3;
+console.log(rem);
+
+//exponential
+
+ex=2**3;
+console.log(ex);
+
+//NaN
+console.log(0/0,typeof 0/0);
