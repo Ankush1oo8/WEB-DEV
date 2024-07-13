dea86329db04dc9d8d07ae0b6b5b8b4c39cddb87
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-07-14 00:55:53 +0530

    DAY22

---
+++ DAY22/app.js
@@ -0,0 +1,11 @@
+const h1=document.getElementById("h1")
+h1.textContent="Ankush"
+
+const list=document.querySelector(".list");
+list.style.backgroundColor="blue";
+list.style.width="300px"
+
+const h11=document.querySelector("h1");
+h11.style.backgroundColor="green"
+
+console.log(list.innerHTML)
