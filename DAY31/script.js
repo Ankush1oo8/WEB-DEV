cea13b05ce8d09e62492acc7575d4bb86da040d8
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-07-15 18:37:18 +0530

    DAY31

--- DAY31/script.js
+++ DAY31/script.js
@@ -8,8 +8,29 @@ app.listen(port,()=>{
     console.log("app is listening on port 3000")
 })
 
-app.use((req,res)=>{
-    console.log("requested");
-    let code="<h1>Ankush</h1>"
-    res.send(code);
+// app.use((req,res)=>{
+//     console.log("requested");
+//     let code="<h1>Ankush</h1>"
+//     res.send(code);
+// })
+
+// sending to multiple routes
+app.get("/",(req,res)=>{
+    res.send("You contacted root path")
+})
+app.get("/apple",(req,res)=>{
+    res.send("You contacted apple path");
+})
+
+// app.get("*",(req,res)=>{
+//     res.send("This Path doesnot exist");
+// })
+app.get("/:username/:id",(req,res)=>{
+    let {username,id}=req.params;
+    res.send(`welcome to the page of @${username}`)
+});
+
+app.get("/search",(req,res)=>{
+    let {q}=req.query;
+    res.send(`${q}`);
 })
