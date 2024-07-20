3a5601f57230f9eed932a8463dad71f04d9c4645
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-07-20 23:20:20 +0530

    DAY41

---
+++ DAY41/app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+//index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+//new route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/newForm.ejs");
+});
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//create route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(8080, () => {
+  console.log("Listening on 8080");
+});
